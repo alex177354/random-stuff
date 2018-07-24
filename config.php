7adaa165f919e0ffc54ec5628f24d4a8899ebc33
alex177354 <alex177354@users.noreply.github.com>
2018-07-23 23:45:19 -0400

    reee
    reee

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = ""; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="Forseen Servers"; // For your Browser Name
+$community_name ="Forseen Servers"; // Your community/server name. The one in the middle
+$community_url = "https://forseengaming.mistforums.com/"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " A long time ago, in a server far,<br> far away...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Welcome to Forseen Servers"; // Above your server name
+// Under Server Name
+$sub_title_2 = "Follow the Rules or there will be consequences"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "Enjoy Your Stay";
+$left_bottom_sub_title = "$community_url for more info";
+$right_bottom_title = "Forseen Servers Clone Wars RP";
+$right_bottom_sub_title = "Founders: Bizzy, Faulties and Burners";
+
+$left_bottom_text = "Our discord https://discord.gg/8HSeStu";
+
+?>
